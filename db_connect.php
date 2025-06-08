711b74575b5a02c6bdf0c5d6d0710bfaa3486d6d
adam007-bit <adamtijjani009@gmail.com>
2025-06-08 16:31:32 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,11 @@
 <?php
-$servername = "mysql.railway.internal"; // Example: containers-us-west-123.railway.app
-$username = "root"; // Example: root
-$password = "PIyCfwPiRkqMHsjjqJdiPVzNqqskBFTK"; // Provided by Railway
-$dbname = "railway"; // Example: railway_db
+$servername = mysql.railway.internal; // Example: containers-us-west-XX.railway.app
+$username = root; // Example: root
+$password = PIyCfwPiRkqMHsjjqJdiPVzNqqskBFTK; // Your database password
+$dbname = railway; // Your database name
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
